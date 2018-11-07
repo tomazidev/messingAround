d6d992353d4fca3133510bd07870f0cfc191b17f
Tomasz Koprowski <tomasz.koprowski@talktalkplc.com>
2018-11-07 23:46:24 +0000

    loan calculator UI

---
+++ loanCalculator/app.js
@@ -0,0 +1,10 @@
+// const amout = document.getElementById('amout');
+// const interest = document.getElementById('interest');
+// const years = document.getElementById('years');
+// const calcBtn = document.querySelector('btn btn-dark btn-block');
+
+// calcBtn.addEventListener('click', calculateLoan);
+
+// function calculateLoan(e) {
+//     amout
+// }
